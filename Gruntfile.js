27440ecdeb1fbc1f01ef3f49702dd605cabcaa74
wixette <wixette@gmail.com>
2021-04-12 20:50:54 +0800

    Builds p5.anims.min.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,13 +15,21 @@ module.exports = function(grunt) {
           dest: 'build/p5.anims.js',
           src: 'src/main.js'
         },
+      },
+      uglify: {
+        mytarget: {
+          files: {
+            'build/p5.anims.min.js': ['build/p5.anims.js']
+          }
+        }
       }
     });
 
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-rollup');
 
-    grunt.registerTask('default', ['jshint', 'rollup']);
+    grunt.registerTask('default', ['jshint', 'rollup', 'uglify']);
     grunt.registerTask('lint', ['jshint']);
-    grunt.registerTask('build', ['jshint', 'rollup']);
+    grunt.registerTask('build', ['jshint', 'rollup', 'uglify']);
 };
